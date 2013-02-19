c7bc24c5defd345d03aee4d95f4387b6805cea64
napina <ville_ruusutie@hotmail.com>
2013-02-19 15:25:49 +0200

    fixed tabs

--- include/reflectos_lua.h
+++ include/reflectos_lua.h
@@ -31,101 +31,100 @@ IN THE SOFTWARE.
 namespace reflectos {
 //-----------------------------------------------------------------------------
 
-
 template<typename T>
 struct LuaClass
 {
-	void register(lua_State* L)
-	{
-		lua_newtable(L);
-		int methods = lua_gettop(L);
+    void register(lua_State* L)
+    {
+        lua_newtable(L);
+        int methods = lua_gettop(L);
 
-		char const className = type_inspect<T>::name();
-		luaL_newmetatable(L, className);
-		int metatable = lua_gettop(L);
+        char const className = type_inspect<T>::name();
+        luaL_newmetatable(L, className);
+        int metatable = lua_gettop(L);
 
-		// store method table in globals so that
-		// scripts can add functions written in Lua.
-		lua_pushstring(L, className);
-		lua_pushvalue(L, methods);
-		lua_settable(L, LUA_GLOBALSINDEX);
+        // store method table in globals so that
+        // scripts can add functions written in Lua.
+        lua_pushstring(L, className);
+        lua_pushvalue(L, methods);
+        lua_settable(L, LUA_GLOBALSINDEX);
 
-		lua_pushliteral(L, "__metatable");
-		lua_pushvalue(L, methods);
-		lua_settable(L, metatable);  // hide metatable from Lua getmetatable()
+        lua_pushliteral(L, "__metatable");
+        lua_pushvalue(L, methods);
+        lua_settable(L, metatable);  // hide metatable from Lua getmetatable()
 
-		lua_pushliteral(L, "__index");
-		lua_pushvalue(L, methods);
-		lua_settable(L, metatable);
+        lua_pushliteral(L, "__index");
+        lua_pushvalue(L, methods);
+        lua_settable(L, metatable);
 
-		lua_pushliteral(L, "__tostring");
-		lua_pushcfunction(L, toString);
-		lua_settable(L, metatable);
+        lua_pushliteral(L, "__tostring");
+        lua_pushcfunction(L, toString);
+        lua_settable(L, metatable);
 
-		lua_pushliteral(L, "__gc");
-		lua_pushcfunction(L, garbageCollect);
-		lua_settable(L, metatable);
+        lua_pushliteral(L, "__gc");
+        lua_pushcfunction(L, garbageCollect);
+        lua_settable(L, metatable);
 
-		lua_newtable(L);                // mt for method table
-		int mt = lua_gettop(L);
-		lua_pushliteral(L, "new");
-		lua_pushcfunction(L, construct);
-		lua_settable(L, methods);       // add new to method table
-		lua_settable(L, mt);            // mt.__call = new_T
-		lua_setmetatable(L, methods);
+        lua_newtable(L);                // mt for method table
+        int mt = lua_gettop(L);
+        lua_pushliteral(L, "new");
+        lua_pushcfunction(L, construct);
+        lua_settable(L, methods);       // add new to method table
+        lua_settable(L, mt);            // mt.__call = new_T
+        lua_setmetatable(L, methods);
 
 #if 0
-		// fill method table with methods from class T
-		for (RegType *l = T::methods; l->name; l++) {
-			/* edited by Snaily: shouldn't it be const RegType *l ... ? */
-			lua_pushstring(L, l->name);
-			lua_pushlightuserdata(L, (void*)l);
-			lua_pushcclosure(L, thunk, 1);
-			lua_settable(L, methods);*/
-		}
+        // fill method table with methods from class T
+        for (RegType *l = T::methods; l->name; l++) {
+            /* edited by Snaily: shouldn't it be const RegType *l ... ? */
+            lua_pushstring(L, l->name);
+            lua_pushlightuserdata(L, (void*)l);
+            lua_pushcclosure(L, thunk, 1);
+            lua_settable(L, methods);*/
+        }
 #endif
 
-		lua_pop(L, 2);  // drop metatable and method table
-	}
+        lua_pop(L, 2);  // drop metatable and method table
+    }
 
-	static int construct(lua_State* L)
-	{
-		lua_remove(L, 1);
-		void* obj = lua_newuserdata(L, type_inspect<T>::size());
-		type_inspect<T>::constructInPlace(obj);
-		luaL_getmetatable(L, type_inspect<T>::name());
-		lua_setmetatable(L, -2);
-		return 1;
-	}
+    static int construct(lua_State* L)
+    {
+        lua_remove(L, 1);
+        void* obj = lua_newuserdata(L, type_inspect<T>::size());
+        type_inspect<T>::constructInPlace(obj);
+        luaL_getmetatable(L, type_inspect<T>::name());
+        lua_setmetatable(L, -2);
+        return 1;
+    }
 
-	static int garbageCollect(lua_State* L)
-	{
-		void* obj = lua_touserdata(L, 1);
-		type_inspect<T>::destroy(obj);
-		return 0;
-	}
+    static int garbageCollect(lua_State* L)
+    {
+        void* obj = lua_touserdata(L, 1);
+        type_inspect<T>::destroy(obj);
+        return 0;
+    }
 
-	static int toString(lua_State* L)
-	{
-		char buffer[32];
-		void* obj = lua_touserdata(L, 1);
-		sprintf(buff, "%p", obj);
-		lua_pushfstring(L, "%s (%s)", type_inspect<T>::name(), buffer);
-		return 1;
-	}
+    static int toString(lua_State* L)
+    {
+        char buffer[32];
+        void* obj = lua_touserdata(L, 1);
+        sprintf(buff, "%p", obj);
+        lua_pushfstring(L, "%s (%s)", type_inspect<T>::name(), buffer);
+        return 1;
+    }
 };
 
 
 struct LuaBinder
 {
-	LuaBinder(lua_State* lua) : m_lua(lua) {}
-	lua_State* m_lua;
+    LuaBinder(lua_State* lua) : m_lua(lua) {}
+    lua_State* m_lua;
 
-	struct Foo
-	{
-	};
-
-	
+    template<typename T>
+    void visitClass()
+    {
+        LuaClass<T>::register(m_lua);
+    }
 
     template<typename T,typename F>
     void visitField(const char* name, T const* object, F const* field)
